a0da35c1c528db9113f5106ea18aa015b43e2dc5
anc <andychan19910613@gmail.com>
2024-11-01 14:37:06 +0000

    adding GB model to app

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -4,7 +4,7 @@ import pickle
 import matplotlib.pyplot as plt
 
 # Load the trained model
-with open('linear_regression_model.pkl', 'rb') as model_file:
+with open('gradient_boosting_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
 # Streamlit App
@@ -13,9 +13,7 @@ st.title("Crime Rate Predictor")
 # Display Data Dictionary
 st.subheader("Data Dictionary")
 data_dictionary = {
-    'state': 'The state for which the data is provided',
     'year': 'The year of the data record',
-    'total_pop': 'Total population of the state',
     'median_income': 'Median income of the population',
     'poverty_rate': 'Percentage of the population living in poverty',
     'unemployment_rate': 'Unemployment rate in the state',
@@ -29,7 +27,6 @@ data_dictionary = {
     'youth_not_in_school': 'Number of youth not in school',
     'youth_in_foster_care': 'Number of youth in foster care',
     'youth_living_in_poverty': 'Number of youth living in poverty',
-    'youth_school_poverty_interaction': 'Interaction term representing youth not in school and poverty levels'
 }
 st.write(pd.DataFrame(list(data_dictionary.items()), columns=['Feature', 'Description']))
 
@@ -42,8 +39,6 @@ st.dataframe(dataset)
 st.sidebar.header("User Input")
 
 def user_input_features():
-    state = st.sidebar.selectbox('Select State', ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'])
-    total_pop = st.sidebar.number_input('Total Population', min_value=0, value=5000000)
     median_income = st.sidebar.number_input('Median Income', min_value=0, value=60000)
     poverty_rate = st.sidebar.number_input('Poverty Rate (%)', min_value=0.0, max_value=1.0, value=0.15)
     unemployment_rate = st.sidebar.number_input('Unemployment Rate (%)', min_value=0.0, max_value=1.0, value=0.05)
@@ -57,16 +52,14 @@ def user_input_features():
     youth_not_in_school = st.sidebar.number_input('Youth Not in School', min_value=0, value=10000)
     youth_in_foster_care = st.sidebar.number_input('Youth in Foster Care', min_value=0, value=500)
     youth_living_in_poverty = st.sidebar.number_input('Youth Living in Poverty', min_value=0, value=20000)
-    youth_school_poverty_interaction = st.sidebar.number_input('Youth School Poverty Interaction', min_value=0, value=3000000)
 
     data = {
-        'state': [str(state)], 'year': [2022], 'total_pop': [total_pop], 'median_income': [median_income],
+        'year': [2022], 'median_income': [median_income],
         'poverty_rate': [poverty_rate], 'unemployment_rate': [unemployment_rate], 'unemployed_15_weeks': [unemployed_15_weeks],
         'labor_force_participation_rate': [labor_force_participation_rate], 'hs_grad_rate': [hs_grad_rate],
         'bachelors_grad_rate': [bachelors_grad_rate], 'zhvi': [zhvi], 'crude_rate_suicide': [crude_rate_suicide],
         'crude_rate_od': [crude_rate_od], 'youth_not_in_school': [youth_not_in_school],
         'youth_in_foster_care': [youth_in_foster_care], 'youth_living_in_poverty': [youth_living_in_poverty],
-        'youth_school_poverty_interaction': [youth_school_poverty_interaction]
     }
     return pd.DataFrame(data)
 
